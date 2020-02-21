28b2245658feee00d56449e8416960b9dfbed89f
Molly Stone <molly.stone89@gmail.com>
2020-02-20 21:29:05 -0500

    Create random photo method in service

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -5,6 +5,18 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class PhotoService {
+    rootUrl = 'https://api.unsplash.com/';
+    randomEndpoint = '/photos/random';
+    accessKey = 'aPKa_ZHkqgYW1JT6jxSvOilV_lwQO9nU4aSK7DjJZOU';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
+
+    public getRandomPhoto() {
+        const url = `${this.rootUrl}${this.randomEndpoint}`;
+        return this.http.get(url, {
+            headers: {
+                Authorization: `Client-ID ${this.accessKey}`
+            }
+        });
+    }
 }
