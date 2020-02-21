1c2bbc658c878c64ab561611fffa99ad3c0c6c34
Molly Stone <molly.stone89@gmail.com>
2020-02-20 21:04:12 -0500

    Import httpclientmodule and inject http client into photo service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PhotoComponent } from './photo/photo.component';
@@ -10,7 +11,8 @@ import { PhotoComponent } from './photo/photo.component';
     PhotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
